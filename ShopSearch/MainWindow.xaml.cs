d9e6534c430b84ec0fbd3a200dc8b0f65330c7aa
Farid Huseynov <F.H189@hotmail.com>
2018-11-11 15:29:47 +0400

    Search engine works: -Have to write labels on boxes to know what the boxes are showing -Have to write discount function -Have to show the price -Check whether can add message box for the full information

--- ShopSearch/MainWindow.xaml.cs
+++ ShopSearch/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,49 @@ namespace ShopSearch
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<Product> List = new ObservableCollection<Product>();
+        public string ConnectionString { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            searchResult.ItemsSource = List;
+        }
+
+        private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            List.Clear();
+            var request = searchBox.Text;
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    var query = $"select * from Product where Product.ProductName like '{request}%'";
+                    var command = new SqlCommand(query, connection);
+                    var reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var product = new Product();
+                            product.Id = reader.GetInt32(0);
+                            product.ProductName = reader.GetString(1);
+                            product.SupplierId = reader.GetInt32(2);
+                            product.UnitPrice = reader.GetDecimal(3);
+                            product.Package = reader.GetString(4);
+                            product.IsDiscounted = reader.GetBoolean(5);
+                            List.Add(product);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
         }
     }
 }
---
+++ ShopSearch/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopSearch
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public string Package { get; set; }
+        public int SupplierId { get; set; }
+        public bool IsDiscounted { get; set; }
+        public override string ToString()
+        {
+            return $"{ProductName}";
+        }
+    }
+}
