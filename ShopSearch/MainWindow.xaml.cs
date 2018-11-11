db04c0cf5b3389cfedefde3cb64828b7bb9a7b2c
Farid Huseynov <F.H189@hotmail.com>
2018-11-11 18:25:31 +0400

    All done, only labeling required

--- ShopSearch/MainWindow.xaml.cs
+++ ShopSearch/MainWindow.xaml.cs
@@ -25,6 +25,7 @@ namespace ShopSearch
     {
         ObservableCollection<Product> List = new ObservableCollection<Product>();
         public string ConnectionString { get; set; }
+        public string discountStatus = "(0),(1)";
         public MainWindow()
         {
             InitializeComponent();
@@ -34,6 +35,10 @@ namespace ShopSearch
 
         private void searchBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            if (discountCheckBox.IsChecked == true)
+                discountStatus = "(1)";
+            else
+                discountStatus = "(0),(1)";
             List.Clear();
             var request = searchBox.Text;
             using (var connection = new SqlConnection(ConnectionString))
@@ -41,7 +46,7 @@ namespace ShopSearch
                 try
                 {
                     connection.Open();
-                    var query = $"select * from Product where Product.ProductName like '{request}%'";
+                    var query = $"select * from Product where Product.ProductName like '{request}%' and Product.IsDiscontinued in ({discountStatus})";
                     var command = new SqlCommand(query, connection);
                     var reader = command.ExecuteReader();
                     if (reader.HasRows)
@@ -72,9 +77,48 @@ namespace ShopSearch
         {
             var product = searchResult.SelectedItem as Product;
             if (product != null)
-                priceBox.Text = product.UnitPrice.ToString();
+                priceBox.Text = product.UnitPrice.ToString()+" USD";
             else
                 priceBox.Text = "";
+        }
+
+        private void discountCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            List.Clear();
+            var request = searchBox.Text;
+            if (discountCheckBox.IsChecked == true)
+                discountStatus = "(1)";
+            else
+                discountStatus = "(0),(1)";
+            using (var connection = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    var query = $"select * from Product where Product.ProductName like '{searchBox.Text}%' and Product.IsDiscontinued in ({discountStatus})";
+                    var command = new SqlCommand(query, connection);
+                    var reader = command.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var product = new Product();
+                            product.Id = reader.GetInt32(0);
+                            product.ProductName = reader.GetString(1);
+                            product.SupplierId = reader.GetInt32(2);
+                            product.UnitPrice = reader.GetDecimal(3);
+                            product.Package = reader.GetString(4);
+                            product.IsDiscounted = reader.GetBoolean(5);
+                            List.Add(product);
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
     }
 }
